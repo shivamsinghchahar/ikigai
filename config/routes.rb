60d390756667b1f5816aed1e08d12b7f74e999b2
Shivam Chahar <chahar.shivam@yahoo.com>
2020-07-29 22:28:28 +0530

    Added routes for users and sessions

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,12 @@
 Rails.application.routes.draw do
   root 'home#index', as: :home
+
+  namespace :api do
+    namespace :v1 do
+      resource :sessions, only: %i[create show]
+      resources :users, only: %i[create show destroy]
+    end
+  end
+
   get '*path', to: 'home#index', via: :all
 end
