d971eda55b6954c02f406625ef2f2560ec24c5d3
Shivam Chahar <chahar.shivam@yahoo.com>
2020-07-29 22:26:41 +0530

    Added jwt lib

---
+++ lib/json_web_token.rb
@@ -0,0 +1,15 @@
+class JsonWebToken
+  SECRET_KEY = ENV['JWT_SECRET_KEY'] || Rails.application.secrets.secret_key_base
+
+  def self.encode(payload, exp = 24.hours.from_now)
+    payload[:exp] = exp.to_i
+    JWT.encode(payload, SECRET_KEY)
+  end
+
+  def self.verify(token)
+    body = JWT.decode(token, SECRET_KEY).first
+    HashWithIndifferentAccess.new body
+  rescue JWT::VerificationError, JWT::DecodeError
+    nil
+  end
+end
